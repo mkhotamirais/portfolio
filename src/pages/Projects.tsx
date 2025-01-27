fd52ad98605881144d5fca0ddbe20409259cc63a
mkhotamirais <mkhotamirais@gmail.com>
2025-01-27 16:04:24 +0700

    skip dulu

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaChevronDown, FaMagnifyingGlass } from "react-icons/fa6";
 import { projects } from "../locales/all/common";
 import { ProjectsData } from "../types";
 import React, { ChangeEvent, MouseEvent, useState } from "react";
@@ -10,9 +10,14 @@ export default function Projects() {
   const [cari, setCari] = useState("");
   const [selectedBadge, setSelectedBadge] = useState<string[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [openAcc, setOpenAcc] = useState<number | null>(null);
 
   const filteredProjects = projects
-    .filter((item) => item["domain-name"].toLowerCase().includes(cari.toLowerCase()))
+    .filter(
+      (item) =>
+        item["domain-name"].toLowerCase().includes(cari.toLowerCase()) ||
+        item.tools.some((tool) => tool.toLowerCase().includes(cari.toLowerCase()))
+    )
     .filter((item) => item.tools[0].toLowerCase().includes(selectedCategory.toLowerCase()))
     .filter((item) => selectedBadge.every((badge) => item.tools.includes(badge)))
     .sort((a, b) => a["domain-name"].toLowerCase().localeCompare(b["domain-name"].toLowerCase()));
@@ -30,71 +35,91 @@ export default function Projects() {
   };
 
   return (
-    <div>
-      <div className="container py-8">
-        <h1 className="title">All Projects</h1>
-        <div className="space-y-2">
-          <div className="relative flex items-center gap-4 w-full sm:w-64">
-            <input
-              value={cari}
-              onChange={(e) => setCari(e.target.value)}
-              type="text"
-              className="border rounded-lg py-2 px-4 pr-10 focus:outline-none w-full"
-              placeholder="Search here.."
-            />
-            <FaMagnifyingGlass className="absolute text-gray-300 text-lg m-3 right-0" />
-          </div>
-          <div className="flex items-center overflow-x-scroll gap-1">
-            <div className="flex gap-1">
-              {categories.map((item, i) => (
-                <React.Fragment key={i}>
-                  <input
-                    type="radio"
-                    name="category"
-                    value={item === "all categories" ? "" : item}
-                    id={item}
-                    onChange={onCategory}
-                    className="hidden"
-                  />
-                  <label
-                    htmlFor={item}
-                    className={`${
-                      selectedCategory === (item === "all categories" ? "" : item) ? "bg-blue-500 text-white" : ""
-                    } badge`}
-                  >
-                    {item}
-                  </label>
-                </React.Fragment>
-              ))}
-            </div>
-          </div>
-          <div className="flex items-center overflow-x-scroll gap-1">
-            <button type="button" onClick={() => setSelectedBadge([])} className="badge">
-              Reset Tags
-            </button>{" "}
-            :
-            <div className="gap-1 flex">
-              {badges.map((item, i) => (
-                <button
-                  type="button"
-                  key={i}
-                  className={`${selectedBadge.includes(item) ? "bg-blue-500 text-white" : ""} badge`}
-                  onClick={onBadge}
+    <div className="px-4 max-w-4xl mx-auto py-8">
+      <div className="flex items-center justify-between gap-4 mb-6">
+        <h1 className="title min-w-max !mb-0">All Projects</h1>
+        <div className="relative flex items-center gap-4 w-full sm:w-64">
+          <input
+            value={cari}
+            onChange={(e) => setCari(e.target.value)}
+            type="text"
+            className="border rounded-lg py-2 px-4 pr-10 focus:outline-none w-full"
+            placeholder="Search here.."
+          />
+          <FaMagnifyingGlass className="absolute text-gray-300 text-lg m-3 right-0" />
+        </div>
+      </div>
+      <div className="relative space-y-2">
+        <div className="flex items-center overflow-x-scroll gap-1">
+          <div className="flex gap-1">
+            {categories.map((item, i) => (
+              <React.Fragment key={i}>
+                <input
+                  type="radio"
+                  name="category"
+                  value={item === "all categories" ? "" : item}
+                  id={item}
+                  onChange={onCategory}
+                  className="hidden"
+                />
+                <label
+                  htmlFor={item}
+                  className={`${
+                    selectedCategory === (item === "all categories" ? "" : item) ? "bg-blue-500 text-white" : ""
+                  } badge`}
                 >
                   {item}
-                </button>
-              ))}
-            </div>
+                </label>
+              </React.Fragment>
+            ))}
           </div>
         </div>
-        <div className="mt-6">
-          {filteredProjects.map((project: ProjectsData, i) => (
-            <div key={i} className="border rounded-md p-4 overflow-x-scroll">
-              <h3 className="text-2xl font-semibold ">{project["domain-name"]}</h3>
-              <p className="hidden">{project["web-description"]}</p>
-            </div>
-          ))}
+        <div className="flex items-center overflow-x-scroll gap-1">
+          {/* Blur effect on the right */}
+          <div className="absolute top-0 right-0 h-full w-12 bg-gradient-to-l from-white via-white/60 to-transparent pointer-events-none"></div>{" "}
+          <button type="button" onClick={() => setSelectedBadge([])} className="badge">
+            Reset Tags
+          </button>{" "}
+          :
+          <div className="gap-1 flex">
+            {badges.map((item, i) => (
+              <button
+                type="button"
+                key={i}
+                className={`${selectedBadge.includes(item) ? "bg-blue-500 text-white" : ""} badge`}
+                onClick={onBadge}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
         </div>
+      </div>
+      <div className="mt-6">
+        {filteredProjects.map((project: ProjectsData, i) => (
+          <div
+            key={i}
+            onMouseEnter={() => setOpenAcc(i)}
+            onMouseLeave={() => setOpenAcc(null)}
+            className="border mb-2 rounded-md p-4 overflow-x-scroll"
+          >
+            <div className="title flex items-center justify-between">
+              <div className="">
+                <div className="">
+                  <a href={`https://${project["domain-name"]}`} className="text-blue-500 mr-2">
+                    Visit
+                  </a>
+                  <span className="text-lg font-medium">{project["domain-name"]}</span>
+                </div>
+                <div className="text-sm">{project.tools.join(" - ")}</div>
+              </div>
+              <FaChevronDown className={`${openAcc === i ? "rotate-180" : ""} transition-all`} />
+            </div>
+            <div className={`${openAcc === i ? "active" : ""} overflow-hidden acc-project-desc transition-all`}>
+              <p className="mt-2">{project["web-description"]}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
